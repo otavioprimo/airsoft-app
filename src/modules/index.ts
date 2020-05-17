c44dc7fa707ccde3d0b3c0feb4c3ff9ba2566f88
Otávio Primo <otavioprimo@hotmail.com>
2020-05-17 11:51:48 -0300

    implement offline queue

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,7 +1,9 @@
 import {combineReducers} from 'redux';
 import {all} from 'redux-saga/effects';
+import {reducer as offline} from 'redux-offline-queue';
 
 import authRedux from './auth/redux';
+import coreRedux from './core/redux';
 
 import {PersistPartial} from 'redux-persist/es/persistReducer';
 import {AuthState} from '~/modules/auth/redux/types';
@@ -11,9 +13,10 @@ export interface AplicationState extends PersistPartial {
 }
 
 export const rootReducers = combineReducers({
+  offline,
   auth: authRedux.reducer,
 });
 
 export const rootSagas = function* rootSagas() {
-  return yield all([authRedux.sagas()]);
+  return yield all([authRedux.sagas(), coreRedux.sagas()]);
 };
